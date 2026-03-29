4384a40a7c9cebef8666bce20ab74b5ec1e1d950
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-03-29 21:46:05 +0300

    add semaphore mempool control

--- src/main/java/org/blockchain/transaction/Mempool.java
+++ src/main/java/org/blockchain/transaction/Mempool.java
@@ -2,16 +2,22 @@ package org.blockchain.transaction;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public final class Mempool {
     private static final int MEMPOOL_CAP = 100000;
     private static final int TRANSACTIONS_PER_BLOCK_CAP = 1000;
 
     private static final Mempool POOL = new Mempool();
-    private final BlockingDeque<Transaction> transactionsPool = new LinkedBlockingDeque<>(MEMPOOL_CAP);
+    private final PriorityBlockingQueue<Transaction> transactionsPool =
+            new PriorityBlockingQueue<>(
+                    MEMPOOL_CAP,
+                    Comparator.comparingDouble(Transaction::getFee).reversed()
+            );
+    private final Semaphore semaphore = new Semaphore(MEMPOOL_CAP);
 
     private Mempool() {
     }
@@ -21,6 +27,7 @@ public final class Mempool {
     }
 
     public void put(Transaction transaction) throws InterruptedException {
+        semaphore.acquire();
         transactionsPool.put(transaction);
     }
 
@@ -30,8 +37,8 @@ public final class Mempool {
 
         for (int i = 0; i < count; i++) {
             transactionsToBeVerified.add(transactionsPool.poll());
+            semaphore.release();
         }
-
         return transactionsToBeVerified;
     }
 
